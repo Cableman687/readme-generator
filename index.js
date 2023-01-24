2508c6ae03fd0d70afb69f3549d8865158d8c944
William <will_cable@hotmail.com>
2023-01-24 11:48:01 +0800

    Addition of Licence summaries to be displayed beneath each licence badge upon generation

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-var result = "";
-
 var chooseLicence = function(response){
     switch(response.license){
        
-        case 'None' : return "No License";
-        case 'Apache License 2.0' : return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-        case 'GNU General Public License v3.0' : return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-        case 'MIT License' : return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-        case 'Boost Software License 1.0' : return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-        case 'GNU Affero General Public License v3.0' : return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
-        case 'GNU Lesser General Public License v3.0' : return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://opensource.org/licenses/lgpl-3.0)";
-        case 'Mozilla Public License 2.0' : return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-        case 'The Unlicense' : return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+        case 'None' :
+            return "No License";
+        case 'Apache License 2.0' : 
+            var url = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n"
+            var details = "A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
+            var result = [url, details];
+            return result;
 
-        default : return "None";
+        case 'GNU General Public License v3.0' :
+            var url = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n"
+            var details = "Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.";
+            var result = [url, details];
+             return result;
+
+
+        case 'MIT License' :
+            var url = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n"
+            var details = "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
+            var result = [url, details];
+            return result;
+
+        case 'Boost Software License 1.0' :
+            var url = "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)\n"
+            var details = "A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
+            var result = [url, details];
+             return result;
+
+        case 'GNU Affero General Public License v3.0' :
+            var url = "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)\n"
+            var details = "Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.";
+            var result = [url, details];
+             return result;
+
+        case 'GNU Lesser General Public License v3.0' :
+            var url = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://opensource.org/licenses/lgpl-3.0)\n"
+            var details = "Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work.";
+            var result = [url, details];
+             return result;
+
+        case 'Mozilla Public License 2.0' :
+            var url = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)\n"
+            var details = "Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.";
+            var result = [url, details];
+             return result;
+
+        case 'The Unlicense' :
+            var url = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)\n"
+            var details = "A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.";
+            var result = [url, details];
+             return result;
+
+        default :
+         return "No License";
         
     }
 }
@@ -121,8 +161,8 @@ ${response.testing}
     
 ## Questions
 If you have any questions you would like to ask about this project, please reach out using my email below!
-Otherwise, 
 ${response.email}
+ 
 Otherwise, please feel free to check out my full profile!:
 https://github.com/${response.username}
 `
